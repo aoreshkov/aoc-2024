247f011823c9b3b17d55b92ac779da0c8b934138
Atanas Oreshkov <aoreshkov@gmail.com>
2024-12-02 20:35:07 +0200

    Day 1: Historian Hysteria

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,29 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val left = input.map { it.split(" ").first().toInt() }.sorted()
+        val right = input.map { it.split(" ").last().toInt() }.sorted()
+
+        return left.foldIndexed(0) { index, acc, i -> acc + (i - right[index]).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val left = input.map { it.split(" ").first().toInt() }
+        val right = input.map { it.split(" ").last().toInt() }
+
+        return left.fold(0) { sum, element -> sum + element * right.filter { it == element }.size }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+
+    check(part2(testInput) == 31)
+
     part2(input).println()
 }
